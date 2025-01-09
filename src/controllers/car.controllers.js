d37d374d87e8183493f71ff72fc2e86b95059c16
jimmy arenas <j3jimmy@gmail.com>
2025-01-09 18:12:52 -0500

    advances

--- src/controllers/car.controllers.js
+++ src/controllers/car.controllers.js
@@ -6,6 +6,19 @@ const getAll = catchError(async(req, res) => {
     return res.json(cars)
 });
 
+const create = catchError(async(req, res) => {
+    const { brand, model, year, color,  isAvailable } = req.body;
+    const car = await Car.create({
+        brand: brand,
+        model: model,
+        color: color,
+        year: year,
+        isAvailable: isAvailable,
+    });
+    return res.status(201).json(car);
+});
+
 module.exports = {
-    getAll
+    getAll,
+    create,
 }
--- src/routes/car.router.js
+++ src/routes/car.router.js
@@ -1,9 +1,10 @@
-const { getAll } = require('../controllers/car.controllers');
+const { getAll, create } = require('../controllers/car.controllers');
 const express = require('express');
 
 const carRouter = express.Router();
 
 carRouter.route("/cars")
 		.get(getAll)
+		.post(create);
 
 module.exports = carRouter;
